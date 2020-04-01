7bb8c6641cc9c62e9ec63435dc936d1dd8d41932
Akinzou <wiktorjn@gmail.com>
2020-04-01 12:11:02 +0200

    Add comment

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ now = 0
 
 def healAndUp():
     for i in range (0,3):
+        #use health potion
         pydirectinput.press("1")
+        #pick up item
         pydirectinput.press("`")
 
 
@@ -14,9 +16,11 @@ while 1:
     pydirectinput.keyDown("space")
     for i in range(len(keys)):
         pydirectinput.keyDown("space")
+        #Turning
         pydirectinput.press(keys[i])
         healAndUp()
     healAndUp()
+
     #AntyBOT {
     pydirectinput.moveTo(1000, 1000)
     time.sleep(0.5)
